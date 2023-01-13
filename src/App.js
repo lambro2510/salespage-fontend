54211159c2eba6d5ca2349a6051aa01a77b60cba
lam <lambro25102001@gmail.com>
2023-01-13 15:53:42 +0700

    test img

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <TopMenu/>
+      <img src="https://photos.google.com/search/_tra_/photo/AF1QipM8vmdlDRsDA90WEBE3EwFvR2RCcay0OgdE2XSU" alt="Image" width="200" height="300">
     </div>
   );
 }
