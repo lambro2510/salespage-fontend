ccb776cab19b35cfdd9c61abd653224b4fa8407a
lam <lambro25102001@gmail.com>
2023-05-14 10:58:25 +0700

    save

--- src/module/seller/product/sellerProductModal/index.js
+++ src/module/seller/product/sellerProductModal/index.js
@@ -6,13 +6,14 @@ const { TextArea } = Input;
 const { Option } = Select;
 
 const SellerProductModal = ({ product, visible, setVisible }) => {
-    const [editedProduct, setEditedProduct] = React.useState(product);
-    const [productType, setProductType] = React.useState(useSelector((state) => state.type.productType));
+    const [editedProduct, setEditedProduct] = React.useState({ ...product });
+    const [productTypes, setProductTypes] = React.useState([]);
     const stores = useSelector((state) => state.store.sellerStore);
-
+    const types = useSelector((state) => state.type.productType);
     React.useEffect(() => {
-        console.log(editedProduct);
-    })
+        setProductTypes(types);
+        setEditedProduct({ ...product });
+    }, [product])
     const handleCancel = () => {
         setVisible(false);
     };
@@ -42,71 +43,71 @@ const SellerProductModal = ({ product, visible, setVisible }) => {
     };
 
     return (
-            <Modal
-                title="Edit Product"
-                visible={visible}
-                onCancel={handleCancel}
-                footer={[
-                    <Button key="cancel" onClick={handleCancel}>
-                        Cancel
-                    </Button>,
-                    <Button key="save" type="primary" onClick={handleSave}>
-                        Save
-                    </Button>,
-                ]}
-            >
-                <Form labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
-                    <Form.Item label="Product Name">
-                        <Input
-                            name="productName"
-                            value={editedProduct.productName}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item label="Product Price">
-                        <Input
-                            type="number"
-                            name="price"
-                            value={editedProduct.price}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item label="Product Type">
-                        <Select
-                            name="type"
-                            value={editedProduct.type}
-                            onChange={handleSelectChange}
-                        >
-                            {productType?.map((type) => (
-                                <Option key={type?.productType} value={type?.typeName}>
-                                    {type?.typeName}
-                                </Option>
-                            ))}
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="Product Description">
-                        <TextArea
-                            name="description"
-                            value={editedProduct.description}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item label="Selling Address">
-                        <Input
-                            name="sellingAddress"
-                            value={editedProduct.sellingAddress}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item label="Default Image URL">
-                        <Input
-                            name="imageUrl"
-                            value={editedProduct.imageUrl}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                </Form>
-            </Modal>
+        <Modal
+            title="Edit Product"
+            visible={visible}
+            onCancel={handleCancel}
+            footer={[
+                <Button key="cancel" onClick={handleCancel}>
+                    Cancel
+                </Button>,
+                <Button key="save" type="primary" onClick={handleSave}>
+                    Save
+                </Button>,
+            ]}
+        >
+            <Form labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+                <Form.Item label="Product Name">
+                    <Input
+                        name="productName"
+                        value={editedProduct.productName}
+                        onChange={handleChange}
+                    />
+                </Form.Item>
+                <Form.Item label="Product Price">
+                    <Input
+                        type="number"
+                        name="productPrice"
+                        value={editedProduct.productPrice}
+                        onChange={handleChange}
+                    />
+                </Form.Item>
+                <Form.Item label="Product Type">
+                    <Select
+                        name="type"
+                        value={productTypes?.filter(item => item?.productType === editedProduct.type)[0]?.typeName}
+                        onChange={handleSelectChange}
+                    >
+                        {productTypes?.map((type) => (
+                            <Option key={type?.productType} value={type?.typeName}>
+                                {type?.typeName}
+                            </Option>
+                        ))}
+                    </Select>
+                </Form.Item>
+                <Form.Item label="Product Description">
+                    <TextArea
+                        name="description"
+                        value={editedProduct.description}
+                        onChange={handleChange}
+                    />
+                </Form.Item>
+                <Form.Item label="Selling Address">
+                    <Input
+                        name="sellingAddress"
+                        value={editedProduct.sellingAddress}
+                        onChange={handleChange}
+                    />
+                </Form.Item>
+                <Form.Item label="Default Image URL">
+                    <Input
+                        name="imageUrl"
+                        value={editedProduct.imageUrl}
+                        onChange={handleChange}
+                    />
+                </Form.Item>
+            </Form>
+        </Modal>
     );
 };
 
