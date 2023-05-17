23d363a28fa9698a6120c6556d15c489e0742c6f
lam <lambro25102001@gmail.com>
2023-05-17 23:47:16 +0700

    save

--- src/module/seller/product/index.js
+++ src/module/seller/product/index.js
@@ -5,9 +5,7 @@ import SellerListProduct from './sellerListProduct'
 const SellerProduct = () => {
     return (
         <div>
-             <SellerProductModal setVisible={setVisible} visible={visible} id={product.productId} />
-            <SearchProductInput handleSearch={fetchData} productFilter={productFilter} setProductFilter={setProductFilter} />
-            <SellerListProduct />
+            
         </div>
     )
 }
